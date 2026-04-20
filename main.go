92b1dd35e39e0d0832c40c4597afa0bb4f39c529
Joonatan :) <tossudev@gmail.com>
2026-04-20 13:26:47 +0300

    lol

---
+++ main.go
@@ -0,0 +1,149 @@
+// Example program that uses blakjack/webcam library
+// for working with V4L2 devices.
+// The application reads frames from device and writes them to stdout
+// If your device supports motion formats (e.g. H264 or MJPEG) you can
+// use it's output as a video stream.
+// Example usage: go run stdout_streamer.go | vlc -
+package main
+
+import "github.com/blackjack/webcam"
+import "github.com/qeesung/image2ascii/convert"
+import "os"
+import "fmt"
+import "sort"
+import "image"
+import "bytes"
+import "image/jpeg"
+import "github.com/nfnt/resize"
+//import "time"
+
+func readChoice(s string) int {
+	var i int
+	for true {
+		print(s)
+		_, err := fmt.Scanf("%d\n", &i)
+		if err != nil || i < 1 {
+			println("Invalid input. Try again")
+		} else {
+			break
+		}
+	}
+	return i
+}
+
+type FrameSizes []webcam.FrameSize
+
+func (slice FrameSizes) Len() int {
+	return len(slice)
+}
+
+//For sorting purposes
+func (slice FrameSizes) Less(i, j int) bool {
+	ls := slice[i].MaxWidth * slice[i].MaxHeight
+	rs := slice[j].MaxWidth * slice[j].MaxHeight
+	return ls < rs
+}
+
+//For sorting purposes
+func (slice FrameSizes) Swap(i, j int) {
+	slice[i], slice[j] = slice[j], slice[i]
+}
+
+func main() {
+	cam, err := webcam.Open("/dev/video0")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer cam.Close()
+
+	format_desc := cam.GetSupportedFormats()
+	var formats []webcam.PixelFormat
+	for f := range format_desc {
+		formats = append(formats, f)
+	}
+
+	println("Available formats: ")
+	for i, value := range formats {
+		fmt.Fprintf(os.Stderr, "[%d] %s\n", i+1, format_desc[value])
+	}
+
+	choice := readChoice(fmt.Sprintf("Choose format [1-%d]: ", len(formats)))
+	format := formats[choice-1]
+
+	fmt.Fprintf(os.Stderr, "Supported frame sizes for format %s\n", format_desc[format])
+	frames := FrameSizes(cam.GetSupportedFrameSizes(format))
+	sort.Sort(frames)
+
+	for i, value := range frames {
+		fmt.Fprintf(os.Stderr, "[%d] %s\n", i+1, value.GetString())
+	}
+	choice = readChoice(fmt.Sprintf("Choose format [1-%d]: ", len(frames)))
+	size := frames[choice-1]
+
+	f, w, h, err := cam.SetImageFormat(format, uint32(size.MaxWidth), uint32(size.MaxHeight))
+	err = cam.SetFramerate(15.0)
+
+	if err != nil {
+		panic(err.Error())
+	} else {
+		fmt.Fprintf(os.Stderr, "Resulting image format: %s (%dx%d)\n", format_desc[f], w, h)
+	}
+
+	println("Press Enter to start streaming")
+	fmt.Scanf("\n")
+	err = cam.StartStreaming()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	timeout := uint32(5) //5 seconds
+	for {
+		//time.Sleep(100 * time.Millisecond)
+
+		err = cam.WaitForFrame(timeout)
+
+		switch err.(type) {
+		case nil:
+		case *webcam.Timeout:
+			fmt.Fprint(os.Stderr, err.Error())
+			continue
+		default:
+			panic(err.Error())
+		}
+
+		frame, err := cam.ReadFrame()
+		if len(frame) != 0 {
+			img, _, err2 := image.Decode(bytes.NewReader(frame))
+			newImage := resize.Resize(160, 0, img, resize.Lanczos3)
+
+			out, _ := os.Create("./img.jpeg")
+			defer out.Close()
+
+			var opts jpeg.Options
+			opts.Quality = 50
+
+			err = jpeg.Encode(out, newImage, &opts)
+
+			if err != nil || err2 != nil {
+				fmt.Println(err, err2)
+			}
+
+			converter := convert.NewImageConverter()
+			imageFilename := "./img.jpeg"
+			convertOptions := convert.DefaultOptions
+			convertOptions.FitScreen = true
+			convertOptions.Colored = false
+			asciiString := converter.ImageFile2ASCIIString(imageFilename, &convertOptions)
+	
+			fmt.Printf("\033[2J")
+			fmt.Println(asciiString)
+
+			//print(".")
+			//os.Stdout.Write(frame)
+			//os.Stdout.Sync()
+		} else if err != nil {
+			panic(err.Error())
+		}
+	}
+}
+
